78cb3ff8edf8c9092ec2de2b237172609d7e7a11
rubensmeirelles <rrubens.meirelles@gmail.com>
2023-01-23 17:57:12 -0300

    First controller and Razor Pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles web MVC Application.";
+            ViewData["Teacher"] = "Nélio Alves";
 
             return View();
         }
